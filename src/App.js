586dfc90f1b65e44d2ead3a51e11bf956406c646
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2021-08-17 17:35:21 +0530

    assignment -1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Component1 from './components/Component1';
+import Component2 from './components/Component2';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Component1 />
+        <img src="https://avatars.githubusercontent.com/u/49114903?v=4" className="App-logo" alt="logo" />
+        <Component2 />
       </header>
     </div>
   );
---
+++ src/components/Component2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Component2() {
+    return (
+        <div>
+            <h6>	&copy;vinothkumar.s@gmail.com</h6>
+        </div>
+    )
+}
+
+export default Component2
